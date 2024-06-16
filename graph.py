4c6e170b23422accd0abf48fc0f87e9e9a8fd90d
chanho890 <58173664+chanho890@users.noreply.github.com>
2024-06-16 14:37:42 +0900

    Update graph.py

--- graph.py
+++ graph.py
@@ -1,3 +1,7 @@
+from cgi import parse_qs
+from template import html
+import matplotlib.pyplot as plt
+
 def application (environ, start_response): 
   if environ ['PATH_INFO'] == '/graph.png:
     try:
